55f8a524c6686c409d84e53f4e069b7ce573fad8
Adam Conybear <aconybear22@gmail.com>
2025-02-13 21:10:48 -0500

    remove provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppLayout } from "@/components/layout/app-layout";
-import { Providers } from "@/components/providers/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers>
-          <AppLayout>{children}</AppLayout>
-        </Providers>
+        <AppLayout>{children}</AppLayout>
       </body>
     </html>
   );
